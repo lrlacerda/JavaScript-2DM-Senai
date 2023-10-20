faf3d7d9cf5826fc134bcba2fcfd8073f917c318
Lucas Ribeiro De Lacerda <32784630803@info.local>
2023-10-20 11:01:06 -0300

    JavaScript

--- ecmaScript/api-local/helloApi.js
+++ ecmaScript/api-local/helloApi.js
@@ -12,12 +12,19 @@ async function buscarEndereco(cep) {
         const endereco = await promise.json();
         console.log(endereco);
 
-        document.getElementById("endereco").value = endereco.logradouro;
-        document.getElementById("cidade").value = endereco.localidade;
-        document.getElementById("estado").value = endereco.uf;
+        preencherCampos({
+            logradouro: endereco.logradouro,
+            localidade: endereco.localidade,
+            uf: endereco.uf,
+        });
         document.getElementById("not-found").innerHTML = "";
-        
     } catch (error) {
         document.getElementById("not-found").innerHTML = "CEP inválido";
     }
+}
+
+function preencherCampos(endereco) {
+    document.getElementById("endereco").value = endereco.logradouro;
+    document.getElementById("cidade").value = endereco.localidade;
+    document.getElementById("estado").value = endereco.uf;
 }
