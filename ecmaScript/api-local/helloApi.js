a47152bac70712a83f4f2f59aea08a6e7e402d9f
Lucas Ribeiro De Lacerda <32784630803@info.local>
2023-10-20 10:54:37 -0300

    JavaScript

--- ecmaScript/api-local/helloApi.js
+++ ecmaScript/api-local/helloApi.js
@@ -1,1 +1,23 @@
-console.log("Hello Api");
+const urlViaCep = "https://viacep.com.br/ws";
+
+function cadastrar(e) {
+    e.preventDefault();
+    alert("Cadastrar...");
+}
+
+async function buscarEndereco(cep) {
+    const resource = `/${cep}/json/`;
+    try {
+        const promise = await fetch(urlViaCep + resource);
+        const endereco = await promise.json();
+        console.log(endereco);
+
+        document.getElementById("endereco").value = endereco.logradouro;
+        document.getElementById("cidade").value = endereco.localidade;
+        document.getElementById("estado").value = endereco.uf;
+        document.getElementById("not-found").innerHTML = "";
+        
+    } catch (error) {
+        document.getElementById("not-found").innerHTML = "CEP inválido";
+    }
+}
